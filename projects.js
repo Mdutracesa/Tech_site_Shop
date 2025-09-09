67967f374933d334db7c2a9317cacac37632a9c7
Mduduzi <mdutracesa@gmail.com>
2025-09-09 21:06:59 +0200

    Delete projects.jsj

--- projects.js
+++
@@ -1,9 +0,0 @@
-fetch("PROJECTS.md")
-  .then(response => response.text())
-  .then(text => {
-    document.getElementById("project-list").innerHTML = marked.parse(text);
-  })
-  .catch(err => {
-    document.getElementById("project-list").innerHTML = "⚠️ Could not load projects.";
-    console.error(err);
-  });
